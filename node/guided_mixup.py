c45533f0ef5153e259690b1bc3e1954b50b81a09
jhChoi1997 <98075849+jhChoi1997@users.noreply.github.com>
2025-11-03 02:28:49 +0000

    guided_mixup

---
+++ node/guided_mixup.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from util.register import register_node
+
+
+@register_node("guided_mixup")
+class GuidedMixup(nn.Module):
+    def __init__(self, s=30, margin=0.7):
+        super().__init__()
+        self.s = s
+        self.margin = margin
+
+    def forward(self, cos_theta: torch.Tensor, mixed_one_hot: torch.Tensor):
+        phi = (
+            mixed_one_hot * torch.cos(torch.arccos(cos_theta) + self.margin)
+            + (1 - mixed_one_hot) * cos_theta
+        )
+        logits = phi * self.s
+        return logits
--- node/nodes.py
+++ node/nodes.py
@@ -62,22 +62,6 @@ class CosineLinear(nn.Module):
         return cosine_similarity
 
 
-@register_node("guided_mixup")
-class GuidedMixup(nn.Module):
-    def __init__(self, s=30, margin=0.7):
-        super().__init__()
-        self.s = s
-        self.margin = margin
-
-    def forward(self, cos_theta: torch.Tensor, mixed_one_hot: torch.Tensor):
-        phi = (
-            mixed_one_hot * torch.cos(torch.arccos(cos_theta) + self.margin)
-            + (1 - mixed_one_hot) * cos_theta
-        )
-        logits = phi * self.s
-        return logits
-
-
 @register_node("multiply")
 class Multiply(nn.Module):
     def __init__(self):
